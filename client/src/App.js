441802ff1706b929f503ef641cd6f702a310299c
dot4qu <dot4qu@virginia.edu>
2021-10-09 12:39:46 -0700

    Revamp homepage to better detail options and recommend venue search

--- client/src/App.js
+++ client/src/App.js
@@ -18,18 +18,36 @@ class App extends Component {
     return (
       <div className="App">
         <div className="flex-column" style={{maxWidth: "66%"}}>
-            <h1>Show Finder</h1>
-            <h3>Whether you're discovering new artists or falling back on old favorites, view who will be playing at your favorite venues soon</h3>
+            <h1>Lorem</h1>
+            <h3>Whether you're discovering new artists or falling back on old favorites, see who will be playing at your favorite venues soon</h3>
             <p>Show Finder aggregates concert listing from four separate music services to provide a complete list of who is playing in your city.</p>
             <p>Choose from one of two options to start your search:</p>
-            <div className="flex-row">
-                <form action="/show-finder/venue-search" method="GET" style={{flexBasis: "100%"}}>
-                    <button type="submit">Search by venue</button>
-                    <p>Select all venues you're interested in from your city to receive an email every Sunday listing who will be playing there the following week. Link your Spotify account for Show Finder to build a playlist of those artists' songs for you to discover new talent.</p>
+            <div className="flex-row slate-rounded-border flex-basis-100" style={{minHeight: "175px", margin: "20px", padding: "10px"}}>
+                <form action="/show-finder/venue-search" method="GET">
+                    <div className="flex-row align-items-center text-align-left">
+                        <div className="flex-col justify-content-center align-items-center">
+                            <div style={{flexGrow: 1}}></div>
+                            <button className="blue-button-border" type="submit">Search by venue</button>
+                            <div className="flex-col" style={{alignItems: "top" }}>
+                                <p style={{color: "#6699FF", margin: 0}}>Most popular option!</p>
+                            </div>
+                        </div>
+                        <div className="flex-col justify-content-center" style={{paddingLeft: "20px"}}>
+                            <p>Select your favorite venues to receive an email every Sunday listing who will be playing there the following week.</p>
+                            <p>Link your Spotify account for Show Finder to build a playlist of those artists' songs for you to discover new talent.</p>
+                        </div>
+                    </div>
                 </form>
-                <form action="/show-finder/spotify-search" method="GET" style={{flexBasis: "100%"}}>
-                    <button type="submit">Search by artist</button>
-                    <p>Select a list of artists from your Spotify playlists to see who will be playing in your city soon.</p>
+            </div>
+            <div className="flex-row slate-rounded-border flex-basis-100" style={{minHeight: "175px", margin: "20px", padding: "10px"}}>
+                <form action="/show-finder/spotify-search" method="GET">
+                    <div className="flex-row align-items-center text-align-left">
+                        <button className="default-button-border" type="submit">Search by artist</button>
+                        <div className="flex-col justify-content-center" style={{paddingLeft: "20px"}}>
+                            <p>Select a list of artists from your Spotify to see who will be playing in your city soon.</p>
+                            <p>This option is good for finding when artists you already know and love will be playing near you</p>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
